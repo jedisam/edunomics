919f6ad3d394c9fd5912362dbe51af48d8bf6fb6
Yididya Samuel <yidisam18@gmail.com>
2020-06-09 14:04:46 -0400

    Landing page

--- src/components/Layout/Landing.js
+++ src/components/Layout/Landing.js
@@ -1,4 +1,5 @@
 import React from "react";
+import cx from "classnames";
 import img from "../../Images/Landing.jpg";
 
 import styles from "./Landing.module.css";
@@ -13,8 +14,12 @@ const Landing = () => {
         <nav className={styles.nav}>
           <p className={styles.item}>Login</p>
           {/* <a href='/'>Click Me</a> */}
-          <p className={styles.item}>Signup</p>
+          <p className={cx(styles.item, styles.signup)}>Signup</p>
         </nav>
+        <section className={styles}>
+          <h2>Where do you want to go today?</h2>
+          <p>Share your ultimate travel list with us</p>
+        </section>
       </div>
     </div>
   );
